fa91391daf2fb48cf54f08371af42a0c0d1347e5
[romi-gomez] <[romina.gomez@nayra.coop]>
2020-06-01 15:27:41 -0300

    Under construction component structure

---
+++ src/components/sections/UnderConstruction.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const UnderConstruction = (props) => {
+    return (
+        <div>Under Construction</div>
+    )
+}
+
+export default UnderConstruction
