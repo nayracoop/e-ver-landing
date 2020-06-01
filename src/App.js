7fa9a0a76441bfa368b5e840966db2eb450dc9f2
unknown <tobias.irigoyen@nayra.coop>
2020-06-01 19:07:12 -0300

    Initial layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import styled, { ThemeProvider } from "styled-components";
+import './assets/styles/reset.css';
 import './assets/styles/main.css';
 import UnderConstruction from './components/sections/UnderConstruction'
+import BackgroundImg from './assets/img/bg.svg'
 
 const theme = {
   colors: {
     text: "#04090d",
     textOpacity: "0.7",
-    background: "#bd8cbf",
+    background: "#fff",
     backgroundOpacity: "0.3",
   },
   fonts: {
@@ -28,7 +30,17 @@ const Container = styled.div`
   position: relative;
   margin: 0 auto;
   padding: 0;
-  width: 100%;
+  width: 90%;
+  min-height: 650px;
+  max-height: 84vh;
+  padding-top: 16vh;
+  background-image: url(${BackgroundImg});
+  background-size: 65%;
+  background-repeat: no-repeat;
+  background-position: 50% 95%;
+  @media (max-width: ${theme.pageWidth.l}px) {
+    background-size: 100%;    
+  }
 `;
 
 class App extends Component {
@@ -39,7 +51,11 @@ class App extends Component {
     return (
       <Container ref={this.container} className="App">
         <ThemeProvider theme={theme}>
-          <UnderConstruction></UnderConstruction>
+          <UnderConstruction 
+            title="e-ver"
+            lead="¡Organizamos tu evento virtual!"
+            text="Sitio en construcción"
+          />
         </ThemeProvider>
       </Container>
     );
