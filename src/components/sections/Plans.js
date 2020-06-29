2f71b4ee3952310a7e01cd66a41c7e0ca4ea6f98
unknown <tobias.irigoyen@nayra.coop>
2020-06-29 17:00:00 -0300

    About section and spacing fixes

--- src/components/sections/Plans.js
+++ src/components/sections/Plans.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import Wrapper from '../layout/Wrapper'
 import SectionContainer from '../layout/SectionContainer'
 import SectionTexts from '../snippets/SectionTexts'
 import PlansGrid from '../snippets/PlansGrid'
 
+const PlanTexts = styled(SectionTexts)`
+    margin-left: auto;
+    margin-right: auto;
+`
 
 const Plans = (props) => {
     return (
         <Wrapper minHeight="100vh">
             <SectionContainer>
-                <SectionTexts sectionTitle={props.sectionTitle} sectionText={props.sectionText} ></SectionTexts>
+                <PlanTexts sectionTitle={props.sectionTitle} sectionText={props.sectionText} ></PlanTexts>
                 <PlansGrid plansList={props.plansList} btnText={props.btnText}></PlansGrid>
             </SectionContainer>
         </Wrapper>
--- src/components/snippets/SectionTexts.js
+++ src/components/snippets/SectionTexts.js
@@ -2,15 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 import Wrapper from '../layout/Wrapper'
 
+const TextsWrapper = styled(Wrapper)`
+    margin: auto auto 40px auto;
+`
 
 const SectionTitle = styled.h1`
     font-size: 2.8rem;
     color: ${ (props) => props.theme.colors.primary}; 
     text-align: center;
-    margin-bottom: 20px;
+    margin-bottom: 25px;
     line-height: 1.15;
     @media (max-width: ${(props) => props.theme.pageWidth.m}px) {
-        font-size: 2rem;
+        font-size: 1.8rem;
     }
 `
 
@@ -22,14 +25,17 @@ const SectionText = styled.p`
     text-align: center;
     max-width: 75%;
     margin: auto;
+    @media (max-width: ${(props) => props.theme.pageWidth.m}px) {
+        font-size: 1.125rem;
+    }
 `
 const SectionTexts = (props) => {
     return (
-        <Wrapper className={props.className} maxWidth="750px">
+        <TextsWrapper className={props.className} maxWidth="750px">
             <SectionTitle> {props.sectionTitle}
             </SectionTitle>
             <SectionText> {props.sectionText}  </SectionText>
-        </Wrapper>
+        </TextsWrapper>
     )
 }
 
